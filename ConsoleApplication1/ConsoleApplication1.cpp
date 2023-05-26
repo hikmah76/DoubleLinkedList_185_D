be5a8e533db8c191df544a0fa7f164bd87a473b4
nur hikmah <nur.hikmah190504@gmail.com>
2023-05-26 17:40:01 +0700

    if (current->next != NULL)   current->next->prev = previous; // step 2  if (previous->next != NULL)   previous->next = current->next; // step 2  else   START = current->next;
     delete current; // step 4
     return true;
    }

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -171,5 +171,22 @@ void DoubleLinkedList::searchData() {
 
 int main() {
 	DoubleLinkedList obj;
-	while
+	while (true) {
+		try {
+			cout << "\nMenu" << endl; 
+			cout << "1. Add a record from the list" << endl; 
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all records in the ascending orderof roll number" << endl;
+			cout << "4. View all records in the descending orderof roll number" << endl; 
+			cout << "5. search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "\nEnter your choice (1-6): "; 
+			cahr ch;
+			cin >> ch; 
+
+			switch (ch) {
+
+			}
+		}
+	}
 }
